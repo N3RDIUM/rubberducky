6b8ac56d3fd3b1987bce35d2531fd61a42036563
n3rdium <n3rdium@gmail.com>
2025-04-27 08:48:14 +0530

    Activating rubber ducky mode

--- src/main.py
+++ src/main.py
@@ -3,13 +3,11 @@ import ollama
 from microphone import recognize
 from tts import speak
 
-# SYSPROMPT = """I'm a programmer. You are my rubber duck.
-# Your responses should be short, concise, insightful and motivating.
-# Your responses should be REALLY short. One sentence, not more than 15 words.
-# You're not allowed to see the code.
-# Be a good listener, give insightful hints."""
-
-SYSPROMPT = "You are a helpful assistant. Your responses are short, not exceeding one sentence of not more than 16 words."
+SYSPROMPT = """I'm a programmer. You are my rubber duck.
+Your responses should be short, concise, insightful and motivating.
+Your responses should be REALLY short. One sentence, not more than 15 words.
+You're not allowed to see the code.
+Be a good listener, give insightful hints."""
 
 client = ollama.Client()
 model = "llama3.2"
